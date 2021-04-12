12612014553fb31b2146ffd9544f421e4e4aa1c1
tln <factorkevin@gmail.com>
2021-04-12 14:03:47 +0300

    Замечания по проекту

--- BillDirect/src/main/java/com/dic/app/BillDirectApplication.java
+++ BillDirect/src/main/java/com/dic/app/BillDirectApplication.java
@@ -5,19 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 
-/**
- * Замечания по проекту:
- * Тесты: почему-то класс без пакета, один тест, в котором много действий + странное получение токена через собственный же логин.
- * В пакете service не сервисы, также с dao.
- * Странное формирование именования апи.
- * Самописная паджинация.
- * System.out в отдельных форах.
- * В методе CustomerServiceImpl.deleteCustomer нет персиста, то есть метод просто не работает (аналогично в ProductServiceImpl.deleteProduct).
- * При наличии CustomerRepository используется EntityManager.
- * Используются интерфейсы - это плюс.
- *
- * Нет целостности видения проекта, местами с ошибками. Вызывает вопросы работа с базой и организация тестов.
- */
+
 @SpringBootApplication
 public class BillDirectApplication {
 
