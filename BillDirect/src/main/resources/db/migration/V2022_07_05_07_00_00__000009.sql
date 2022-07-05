1f8cdf0114f8344ddebe7d578aaea4fa20428e5c
tln <factorkevin@gmail.com>
2022-07-05 07:23:51 +0300

    Добавил недостающие пункты меню и права на них

---
+++ BillDirect/src/main/resources/db/migration/V2022_07_05_07_00_00__000009.sql
@@ -0,0 +1,14 @@
+create or replace procedure drn137_Загрузка_ФИАС is
+begin
+Null;
+end drn137_Загрузка_ФИАС;
+
+create or replace procedure drn90_Редактирование_отчетов is
+begin
+Null;
+end drn90_Редактирование_отчетов;
+
+-- Grant/Revoke object privileges
+grant execute on DRN90_РЕДАКТИРОВАНИЕ_ОТЧЕТОВ to АДМИНИСТРАТОР;
+grant execute on DRN137_ЗАГРУЗКА_ФИАС to АДМИНИСТРАТОР;
+
